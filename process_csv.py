e70c93056bf14e31c9c9517261fcd15fa1e8fffa
Sarka Jana <sarkajana33@gmail.com>
2021-02-13 18:07:13 +0100

    Implement function process_csv()

--- process_csv.py
+++ process_csv.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from typing import Tuple, List
+import csv
 
 
 @dataclass
@@ -23,10 +24,41 @@ class User:
         return f"[USER] {self.first_name} " \
                f"{self.last_name} - {self.phone_number}"
 
+# user1 = User(first_name='Jana', last_name='Jahodová', phone_number='12345678')
+# user2 = User(first_name='Šárka', last_name='Šarlotová', phone_number='12345678')
+# user1.__eq__(user2)
 
 def process_csv(path: str) -> Tuple[List[User], List[str]]:
     # TODO finish this function
-    pass
+    with open ("./users.csv", newline='') as users_file:
+        csv_reader_object = csv.reader(users_file)
+        users_list = []
+        list_of_string = []
+        for count, row in enumerate(csv_reader_object, -1):
+            if csv_reader_object.line_num == 1:
+                continue
+            else:
+                first_name = row[0]     #1. hodnota řádku
+                last_name = row[1]
+                phone_number = row[2]
+                field_name = []
+                if not first_name.isalpha():
+                    field_name.append("invalid first name")
+
+                if not last_name.isalpha():
+                    field_name.append("invalid last name")
+
+                if not phone_number.isdigit():
+                    field_name.append("invalid phone number")
+
+                if first_name.isalpha() and last_name.isalpha() and phone_number.isdigit():
+                    user = User(first_name=first_name, last_name=last_name, phone_number=phone_number)
+                    users_list.append(user)
+                else:
+                    error = ', '.join(field_name)
+                    list_of_string.append(f'[ERROR - row {count}] {error}')
+
+    return tuple((users_list, list_of_string))  #double round brackets
 
 
 def is_correct(users: [User], errors: [str]):
@@ -53,7 +85,7 @@ if is_correct(users, errors):
 # Users should be returned as list of instances of User class
 # Validation rules are simple:
 # First and last name can contain only alphabet letter
-# Phone number can contain only numbers
+# Phone number can contain only numbers - string, ve kterém budou jenom čísla
 # Errors should be returned as list of strings in format
 # (one string for each invalid row)
 # [ERROR - row {row_number}] invalid {field name}, invalid {field name}, ....
